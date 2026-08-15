4b5b4e838d34b8584cb1addd18fa1fadb0a8825f
Atharva <atharvabutte03@gmail.com>
2026-08-15 16:41:06 +0530

    feat(auth): configure SecurityFilterChain with stateless sessions and route access rules

---
+++ backend/src/main/java/com/atharva/workflow/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.atharva.workflow.config;
+
+import com.atharva.workflow.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                                // Public auth routes
+                                .requestMatchers("/api/v1/auth/**").permitAll()
+                                // All other workflow/execution endpoints require valid JWT
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(session-> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
